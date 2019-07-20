ee260863306c34973b6dc2e069ba120036a1f290
openvela-robot <openvela-robot@xiaomi.com>
2019-07-20 14:25:21 +0800

    Version bump for Sprint-30

--- IntegrationBus/source/mw/comadapter/ComAdapter.hpp
+++ IntegrationBus/source/mw/comadapter/ComAdapter.hpp
@@ -19,7 +19,6 @@
 
 // Add connection types here and make sure they are instantiated in ComAdapter.cpp
 #include "FastRtpsConnection.hpp"
-#include "VAsioConnection.hpp"
 
 
 namespace ib {
@@ -98,6 +97,7 @@ public:
     void SendIbMessage(EndpointAddress from, const sim::fr::CycleStart& msg) override;
     void SendIbMessage(EndpointAddress from, const sim::fr::HostCommand& msg) override;
     void SendIbMessage(EndpointAddress from, const sim::fr::ControllerConfig& msg) override;
+    void SendIbMessage(EndpointAddress from, const sim::fr::TxBufferConfigUpdate& msg) override;
     void SendIbMessage(EndpointAddress from, const sim::fr::TxBufferUpdate& msg) override;
     void SendIbMessage(EndpointAddress from, const sim::fr::ControllerStatus& msg) override;
 
@@ -148,9 +148,6 @@ public:
 
     void Run() override { _ibConnection.Run(); }
     void Stop() override { _ibConnection.Stop(); }
-
-    // For Testing Purposes:
-    inline auto GetIbConnection() -> IbConnectionT& { return _ibConnection; }
 
 private:
     // ----------------------------------------
