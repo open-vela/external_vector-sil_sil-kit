bea4b26892481ed1f1a52164ec85aa0ec48f9c18
liujinye <liujinye@xiaomi.com>
2019-08-28 15:18:52 +0800

    VIB-37 Update documentation
    Add more examples. Restructure docs layout.

--- IntegrationBus/source/mw/comadapter/ComAdapter_impl.hpp
+++ IntegrationBus/source/mw/comadapter/ComAdapter_impl.hpp
@@ -59,7 +59,11 @@ ComAdapter<IbConnectionT>::ComAdapter(cfg::Config config, const std::string& par
     // NB: do not create the _logger in the initializer list. If participantName is empty,
     //  this will cause a fairly unintuitive exception in spdlog.
     _logger = spdlog::create<spdlog::sinks::null_sink_st>(_participantName);
-    spdlog::set_default_logger(_logger);
+    // NB: logger gets dropped from registry immediately after creating so that two comAdapter with the same
+    // participantName won't lead to a spdlog exception because a logger with this name does already exist.
+    spdlog::drop(_participantName);
+    // set_default_logger should not be used here, as there can only be one default logger and if another comAdapter
+    // gets created, the first default logger will be dropped from the registry as well.
 }
 
 template <class IbConnectionT>
@@ -455,25 +459,25 @@ void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::f
 }
 
 template <class IbConnectionT>
-void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::LinMessage& msg)
+void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::SendFrameRequest& msg)
 {
     SendIbMessageImpl(from, msg);
 }
 
 template <class IbConnectionT>
-void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::RxRequest& msg)
+void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::SendFrameHeaderRequest& msg)
 {
     SendIbMessageImpl(from, msg);
 }
 
 template <class IbConnectionT>
-void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::TxAcknowledge& msg)
+void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::Transmission& msg)
 {
     SendIbMessageImpl(from, msg);
 }
 
 template <class IbConnectionT>
-void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::WakeupRequest& msg)
+void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::WakeupPulse& msg)
 {
     SendIbMessageImpl(from, msg);
 }
@@ -485,13 +489,13 @@ void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::l
 }
 
 template <class IbConnectionT>
-void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::SlaveConfiguration& msg)
+void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::ControllerStatusUpdate& msg)
 {
     SendIbMessageImpl(from, msg);
 }
 
 template <class IbConnectionT>
-void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::SlaveResponse& msg)
+void ComAdapter<IbConnectionT>::SendIbMessage(EndpointAddress from, const sim::lin::FrameResponseUpdate& msg)
 {
     SendIbMessageImpl(from, msg);
 }
